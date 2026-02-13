9c6f5f84972abb94778faafdb3fae8ca123b0161
Cse21-034 <leatilemanando@gmail.com>
2026-02-13 12:56:17 +0200

    fix5

--- src/components/HeroImageSlider.tsx
+++ src/components/HeroImageSlider.tsx
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const IMAGES = [
-    "/images/hero/1.jpg",
-    "/images/hero/2.jpg",
+    "/images/hero/1.png",
+    "/images/hero/2.png",
     "/images/hero/3.jpg",
-    "/images/hero/4.jpg",
+    "/images/hero/4.png",
     "/images/hero/5.jpeg",
     "/images/hero/6.jpg",
     "/images/hero/7.jpg",
