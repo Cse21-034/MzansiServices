4e58b335f86426153b02a69f1ce9d1ee1d1a66dc
Cse21-034 <leatilemanando@gmail.com>
2026-02-19 16:53:57 +0200

    Remove non-existent MembershipUploadForm import

--- src/app/business/page.tsx
+++ src/app/business/page.tsx
@@ -12,7 +12,6 @@ import Label from "@/components/Label";
 import Badge from "@/shared/Badge";
 import Modal from "@/shared/Modal";
 import BusinessNav from "@/components/BusinessNav";
-import { MembershipUploadForm } from "@/components/MembershipUploadForm";
 import MembershipsList from "@/components/MembershipsList";
 const defaultBusinessImage = "/images/namibia-logo/squarelogo.PNG";
 import {
